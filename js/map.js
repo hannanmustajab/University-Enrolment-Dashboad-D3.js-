5c54e5f30fde9938dc7ea79f3216697dc0d8b9cf
hannanmustajab <hannanmustajab@icloud.com>
2023-01-29 18:53:07 +0100

    line chart not working

--- js/map.js
+++ js/map.js
@@ -28,8 +28,9 @@ svg.call(zoom);
 
 var map = svg.append("g")
     .attr("class", "map");
-console.log('hello');
-d3.queue()
+
+
+    d3.queue()
     .defer(d3.json, "js/50m.json")
     .defer(d3.csv, "../csv/processed/merged_dataset.csv")
     .await(function (error, world, data) {
@@ -52,7 +53,7 @@ function drawMap(world, data) {
     var path = d3.geoPath().projection(projection);
 
     var color = d3.scaleThreshold()
-        .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
+        .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 15000000000])
         .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
     
     var features = topojson.feature(world, world.objects.countries).features;
