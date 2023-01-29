ccaab6f3162b25ca10c818b2fd7eaa346a58640e
hannanmustajab <hannanmustajab@icloud.com>
2023-01-29 15:49:43 +0100

    map changes

--- js/map.js
+++ js/map.js
@@ -54,7 +54,7 @@ function drawMap(world, data) {
     var color = d3.scaleThreshold()
         .domain([10000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000, 500000000, 1500000000])
         .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)", "rgb(33,113,181)", "rgb(8,81,156)", "rgb(8,48,107)", "rgb(3,19,43)"]);
-       
+    
     var features = topojson.feature(world, world.objects.countries).features;
     var populationById = {};
 
@@ -107,9 +107,6 @@ function drawMap(world, data) {
             var nameHeading = "Name : " + d.properties.name;
             d3.select(".name_country").text(nameHeading);
 
-            d3.select(".card_country")
-            .text(d.properties.name);
-            
             var oldUniNameHeading = "Oldest University : " + d.details.university;
             d3.select(".oldest_university")
                 .text(oldUniNameHeading);
@@ -123,9 +120,8 @@ function drawMap(world, data) {
                 .text(d.details.public);
 
             var countofStudents = "Total Number of Students: " + d3.format(".2s")(d.details.count);
-
             d3.select(".count")
-                .text(d3.format(".2s")(d.details.count));
+                .text((countofStudents));
 
             d3.select("#private_uni")
                 .text(d3.format(".2s")(d.details.private_p) + '%');
@@ -133,9 +129,8 @@ function drawMap(world, data) {
             d3.select("#private_uni_progressbar")
                 .style("width", d3.format(".2s")(d.details.private_p) + '%');
 
-            var foundedyear = "Found Year: " + d.details.year;
-            d3.select(".year")
-                .text(foundedyear);
+                d3.select(".year")
+                .text(d.details.year);
 
             d3.select('.details')
                 .style('visibility', "visible")
