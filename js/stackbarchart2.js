7069d43c634015d34af7ab2fa503e3202537aa29
SaadBaig <saad.baig@hotmail.com>
2023-02-05 17:11:36 +0100

    stack bar chart 2 fixed

--- js/stackbarchart2.js
+++ js/stackbarchart2.js
@@ -1,13 +1,13 @@
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 90, left: 30 },
-    width = 670 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width_stack2 = 670 - margin.left - margin.right,
+    height_stack2 = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg_stack2 = d3.select("#stacked_bar2")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width_stack2 + margin.left + margin.right)
+    .attr("height", height_stack2 + margin.top + margin.bottom)
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
@@ -16,8 +16,8 @@ var svg_stack2 = d3.select("#stacked_bar2")
 
 svg_stack2.append("text")
     .attr("text-anchor", "end")
-    .attr("x", width - 300)
-    .attr("y", height + margin.top + 30)
+    .attr("x", width_stack2 - 300)
+    .attr("y", height_stack2 + margin.top + 30)
     .text("Year");
 
 // Y axis label:
@@ -28,22 +28,22 @@ svg_stack2.append("text")
     .attr("x", -margin.top - 80)
     .text("Number of Students")
 
+
+
 // Parse the Data
 d3.csv("js/csv/processed/private_public_by_year_processed.csv", function (data) {
-   
+
     var parsetime = d3.timeParse("%Y-%m-%d");
     data.forEach(function (d) {
         const sum = +d.private + parseFloat(d.public);
         d.year = parsetime(d.year);
         d.private_perc = +(parseFloat(d.private) / sum) * 100;
         d.public_perc = +(parseFloat(d.public) / sum) * 100;
-        
+
     });
     data.forEach(function (d) {
         d.year = d.year.getFullYear();
     });
-
-    console.log(data);
 
     // List of subgroups = header of the csv files = soil condition here
     var subgroups = data.columns.slice(1)
@@ -58,16 +58,16 @@ d3.csv("js/csv/processed/private_public_by_year_processed.csv", function (data) 
     // Add X axis
     var x = d3.scaleBand()
         .domain(groups)
-        .range([0, width])
+        .range([0, width_stack2])
         .padding([0.2])
     svg_stack2.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate(0," + height_stack2 + ")")
         .call(d3.axisBottom(x).tickSizeOuter(0));
 
     // Add Y axis
     var y = d3.scaleLinear()
         .domain([0, 200000000])
-        .range([height, 0]);
+        .range([height_stack2, 0]);
     svg_stack2.append("g")
         .call(d3.axisRight(y));
 
@@ -80,6 +80,37 @@ d3.csv("js/csv/processed/private_public_by_year_processed.csv", function (data) 
     var stackedData = d3.stack()
         .keys(subgroups)
         (data)
+
+
+    // -------------
+    // ADD LEGENDS
+    //--------------
+    // Add one dot in the legend for each name.
+    var size = 10
+    svg_stack2.selectAll("mydots")
+        .data(capitalized)
+        .enter()
+        .append("rect")
+        // .attr("transform", function (d, i) { return "translate(" + i * (100 / color.domain().length) + ",350)"; })
+        .attr("x", 10)
+        .attr("y", function (d, i) { return 355 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
+        .attr("width", size)
+        .attr("height", size)
+        .style("fill", function (d) { return color(d) })
+
+
+    // Add one dot in the legend for each name.
+    svg_stack2.selectAll("mylabels")
+        .data(capitalized)
+        .enter()
+        .append("text")
+        // .attr("transform", function (d, i) { return "translate(" + i * (450 / color.domain().length) + ",350)"; })
+        .attr("x", 10 + size * 1.2)
+        .attr("y", function (d, i) { return 355 + i * (size + 5) + (size / 2) }) // 100 is where the first dot appears. 25 is the distance between dots
+        .style("fill", function (d) { return color(d) })
+        .text(function (d) { return d })
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
 
 
     // ----------------
@@ -99,10 +130,10 @@ d3.csv("js/csv/processed/private_public_by_year_processed.csv", function (data) 
     var mouseover = function (d) {
         var subgroupName = d3.select(this.parentNode).datum().key;
         var subgroupValue = d.data[subgroupName];
-        var PercentageToShow = d.data[subgroupName+'_perc'];
-        
+        var PercentageToShow = d.data[subgroupName + '_perc'];
+
         tooltip
-            .html(subgroupName + "<br>" + d3.format(".2s")(subgroupValue)+ "<br>" + "Percentage: " + d3.format("20.2s")(PercentageToShow)+'%')
+            .html(subgroupName + "<br>" + d3.format(".2s")(subgroupValue) + "<br>" + "Percentage: " + d3.format("20.2s")(PercentageToShow) + '%')
             .style("opacity", 1)
     }
     var mousemove = function (d) {
@@ -114,37 +145,6 @@ d3.csv("js/csv/processed/private_public_by_year_processed.csv", function (data) 
         tooltip
             .style("opacity", 0)
     }
-
-    // -------------
-    // ADD LEGENDS
-    //--------------
-    // Add one dot in the legend for each name.
-    var size = 10
-    svg_stack2.selectAll("mydots")
-        .data(capitalized)
-        .enter()
-        .append("rect")
-        // .attr("transform", function (d, i) { return "translate(" + i * (100 / color.domain().length) + ",350)"; })
-        .attr("x", 10)
-        .attr("y", function (d, i) { return 355 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("width", size)
-        .attr("height", size)
-        .style("fill", function (d) { return color(d) })
-        
-
-    // Add one dot in the legend for each name.
-    svg_stack2.selectAll("mylabels")
-        .data(capitalized)
-        .enter()
-        .append("text")
-        // .attr("transform", function (d, i) { return "translate(" + i * (450 / color.domain().length) + ",350)"; })
-        .attr("x", 10 + size * 1.2)
-        .attr("y", function (d, i) { return 355 + i * (size + 5) + (size / 2) }) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", function (d) { return color(d) })
-        .text(function (d) { return d })
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-
     // Show the bars
     svg_stack2.append("g")
         .selectAll("g")
